14bd03bbb38f1b28541d40b7a814a401e9640710
Michal <michal.wojcieszak.dev@gmail.com>
2022-06-07 19:52:51 +0200

    removed typo

--- database/__init__.py
+++ database/__init__.py
@@ -6,9 +6,9 @@ from flask_login import LoginManager
 from online_chat_app import app
 
 
-app.config['SQLALCHEMY_DATA_BASE_URI'] = 'sqlite:///../database/online_chat.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/online_chat.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)    # encrypts passwords in database
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_page'     # set log in page
-login_manager.login_message = u'Zaloguj sie aby uzyskac dostep'     # message for non-logged in user
+login_manager.login_message = u'Please log in to get access.'     # message for non-logged in user
